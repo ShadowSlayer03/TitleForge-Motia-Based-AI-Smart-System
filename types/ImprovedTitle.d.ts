fc11186298d573c91b2e796fb067cf02fb47b237
ShadowSlayer03 <sslayer441@gmail.com>
2025-11-02 14:15:15 +0530

    Added types and interfaces for the project

---
+++ types/ImprovedTitle.d.ts
@@ -0,0 +1,9 @@
+
+interface ImprovedTitle {
+    original: string;
+    improved: string;
+    rational: string;
+    url: string;
+}
+
+export default ImprovedTitle;
---
+++ types/Video.d.ts
@@ -0,0 +1,10 @@
+
+interface Video {
+    videoId: string;
+    title: string;
+    url: string;
+    publishedAt: string;
+    thumbnail: string;
+}
+
+export default Video;
